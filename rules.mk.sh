48857057f60a05cc2cfde037fd153a2bee1b2bf8
john <john>
1999-06-03 14:14:40 +0000

    new file, builds a Makefile fragment rules.mk that describes how to build dl libraries for the current configuration

---
+++ rules.mk.sh
@@ -0,0 +1,29 @@
+# rules.mk.sh -- Build dynamically-loadable objects for librep
+# $Id$
+
+repdir=$1
+repexecdir=$2
+repdocfile=$3
+
+cat <<EOF
+# rules.mk
+
+repdir=$repdir
+repexecdir=$repexecdir
+repdocfile=$repdocfile
+
+rep_LIBTOOL=\$(repexecdir)/libtool
+
+# use this like:
+# libfoo.la : foo.lo bar.lo
+#	\$(rep_DL_LD) link-opts...
+
+rep_DL_LD=	\$(rep_LIBTOOL) \$(CC) -export-dynamic -rpath \$(repexecdir)
+rep_DL_INSTALL=	\$(rep_LIBTOOL) $(INSTALL)
+rep_DL_UNINSTALL=\$(rep_LIBTOOL) rm
+
+# Rule for libtool controlled C objects
+%.lo : %.c
+	\$(rep_LIBTOOL) \$(CC) -c \$(CPPFLAGS) \$(CFLAGS) \$<
+
+EOF
