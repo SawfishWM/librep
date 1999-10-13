f25fc1be4ad757d3deda9b94fe1b71a35106e1db
john <john>
1999-10-13 19:14:35 +0000

    (main): handle --version option

--- src/rep.c
+++ src/rep.c
@@ -8,7 +8,20 @@
 #endif
 
 #include "rep.h"
+#include "build.h"
 #include <string.h>
+
+static void
+usage (void)
+{
+    fputs ("\
+    --version    print version details\n\
+    --no-rc      don't load rc or site-init files\n\
+    -f FUNCTION  call the Lisp function FUNCTION\n\
+    -l FILE      load the file of Lisp forms called FILE\n\
+    -q           quit\n"
+	   , stderr);
+}
 
 int
 main(int argc, char **argv)
@@ -16,7 +29,13 @@ main(int argc, char **argv)
     char *prog_name = *argv++;
     argc--;
 
-    rep_init(prog_name, &argc, &argv, 0, 0);
+    rep_init(prog_name, &argc, &argv, 0, usage);
+
+    if (rep_get_option ("--version", 0))
+    {
+	printf ("rep version %s\n", REP_VERSION);
+	return 0;
+    }
 
     Fload(rep_string_dup("rep"), Qnil, Qnil, Qnil);
 
