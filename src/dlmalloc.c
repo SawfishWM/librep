1a2b3856093287f45843def36da76966e1504ef0
john <john>
1999-10-27 12:47:53 +0000

    fix sbrk () declaration on FreeBSD (from Yukihiro Nakai <nakai@internetsolutions.co.jp>)

--- src/dlmalloc.c
+++ src/dlmalloc.c
@@ -792,11 +792,9 @@ Void_t *(*__morecore)() = __default_morecore_init;
 
 #else /* INTERNAL_LINUX_C_LIB */
 
-#ifdef __FreeBSD__
-# define ptrdiff_t int
-#endif
-
-#if __STD_C
+#if defined (__FreeBSD__)
+extern char*       sbrk(ptrdiff_t);
+#elif __STD_C
 extern Void_t*     sbrk(ptrdiff_t);
 #else
 extern Void_t*     sbrk();
