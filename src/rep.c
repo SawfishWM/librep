e9241539e9be88ab0834547ecca5dad577720d1f
john <john>
1999-12-31 01:09:34 +0000

    use rep_load_environment () and rep_top_level_exit ()

--- src/rep.c
+++ src/rep.c
@@ -29,7 +29,7 @@ main(int argc, char **argv)
     char *prog_name = *argv++;
     argc--;
 
-    rep_init(prog_name, &argc, &argv, 0, usage);
+    rep_init (prog_name, &argc, &argv, 0, usage);
 
     if (rep_get_option ("--version", 0))
     {
@@ -37,36 +37,7 @@ main(int argc, char **argv)
 	return 0;
     }
 
-    Fload(rep_string_dup("rep"), Qnil, Qnil, Qnil, Qnil);
+    rep_load_environment (rep_string_dup ("rep"));
 
-    if(rep_throw_value && rep_CAR(rep_throw_value) == Qerror)
-    {
-	/* If quitting due to an error, print the error cell if
-	   at all possible. */
-	repv stream = Fstderr_file();
-	repv old_tv = rep_throw_value;
-	rep_GC_root gc_old_tv;
-	rep_PUSHGC(gc_old_tv, old_tv);
-	rep_throw_value = rep_NULL;
-	if(stream && rep_FILEP(stream))
-	{
-	    fputs("error--> ", stderr);
-	    Fprin1(rep_CDR(old_tv), stream);
-	    fputc('\n', stderr);
-	}
-	else
-	    fputs("exiting due to error\n", stderr);
-	rep_throw_value = old_tv;
-	rep_POPGC;
-	return 10;
-    }
-
-    if (rep_throw_value
-	&& rep_CAR (rep_throw_value) == Qquit
-	&& rep_INTP (rep_CDR(rep_throw_value)))
-    {
-	return rep_INT (rep_CDR(rep_throw_value));
-    }
-
-    return 0;
+    return rep_top_level_exit ();
 }
