293f537f0ba54068d26011a494ca87cd429e6bdf
john <john>
2000-02-06 00:11:47 +0000

    removed the thread functions, they're in C code now

--- lisp/threads.jl
+++ lisp/threads.jl
@@ -45,115 +45,42 @@
 ;; (thread-invoke)
 
 
-;; variables
+;; mutexes
 
-;; Each thread is [thread NAME CONTINUATION]
+;; Each mutex is (mutex [OWNING-THREAD [BLOCKED-THREADS...]])
 
-(defvar all-threads nil
-  "List of all runnable threads. The currently running thread is at the head
-of the list, followed by the next-to-run thread, and so on.")
+(defun make-mutex ()
+  "Create and return a mutex object. No thread will own the new mutex."
+  (list 'mutex))
 
-(defvar current-thread nil
-  "The currently running thread, or nil if threaded execution hasn't
-started yet")
+(defun mutexp (arg)
+  "Return `t' if ARG is a mutex object."
+  (eq (car arg) 'mutex))
 
-(defun make-thread (thunk &optional name)
-  "Create and return an object representing a new thread of execution. The
-new thread will begin by calling THUNK, a function with zero parameters.
-If NAME is specified it is taken as the name of the new thread."
-  (or (call/cc (lambda (cont)
-		 (let
-		     ((thread (vector 'thread name cont)))
-		   (setq all-threads (nconc all-threads (list thread)))
-		   thread)))
-      ;; always invoke thread continuations with nil arg
-      (thunk)))
+(defun obtain-mutex (mtx)
+  "Obtain the mutex MTX for the current thread. Will suspend the current
+thread until the mutex is available."
+  (if (null (cdr mtx))
+      (rplacd mtx (list current-thread))
+    (rplacd mtx (nconc (rplacd mtx) (list current-thread)))
+    (thread-suspend (current-thread))))
 
-(defun thread-invoke ()
-  "Pass control to the thread at the head of the list of all threads. This
-function should usually only be called once, to enter threaded execution
-after creating the initial thread(s)."
-  (or all-threads (error "No thread to invoke"))
-  (if current-thread
-      ;; need to save state of current thread
-      (call/cc (lambda (cont)
-		 (aset current-thread 2 cont)
-		 (when (setq current-thread (car all-threads))
-		   ((aref current-thread 2) nil))))
-    (when (setq current-thread (car all-threads))
-      ((aref current-thread 2) nil))))
-
-(defun threadp (arg)
-  "Return `t' if ARG is an object representing a thread of execution."
-  (and (vectorp arg) (>= (length arg) 3) (eq (aref arg 0) 'thread)))
-
-(defun delete-thread (&optional thread)
-  "Remove the thread of execution represented by THREAD. If THREAD is the
-currently active thread, then control will pass to the next thread in the
-queue. If THREAD is undefined, the currently executing thread is deleted."
-  (unless thread
-    (setq thread current-thread))
-  (when thread
-    (setq all-threads (delq thread all-threads))
-    (when (eq current-thread thread)
-      (thread-invoke))))
-
-(defun thread-name (thread)
-  "Return the name of the thread of execution THREAD, or `nil' if THREAD has
-no known name."
-  (aref thread 1))
-
-(defun thread-yield ()
-  "Pass control away from the current thread if other threads are waiting to
-run."
-  (when current-thread
-    (setq all-threads (nconc (delq current-thread all-threads)
-			     (list current-thread))))
-  (when (and all-threads (not (eq (car all-threads) current-thread)))
-    (thread-invoke)))
-
-
-;; semaphores
-
-;; Each semaphore is (semaphore [OWNING-THREAD [BLOCKED-THREADS...]])
-
-(defun make-semaphore ()
-  "Create and return a semaphore object. No thread will own the new semaphore."
-  (list 'semaphore))
-
-(defun semaphorep (arg)
-  "Return `t' if ARG is a semaphore object."
-  (eq (car arg) 'semaphore))
-
-(defun obtain-semaphore (sem)
-  "Obtain the semaphore SEM for the current thread. Will suspend the current
-thread until the semaphore is available."
-  (if (null (cdr sem))
-      (rplacd sem (list current-thread))
-    (rplacd sem (nconc (rplacd sem) (list current-thread)))
-    (setq all-threads (delq current-thread all-threads))
-    (thread-yield)))
-
-(defun maybe-obtain-semaphore (sem)
-  "Attempt to obtain semaphore SEM for the current thread without blocking.
-Returns `t' if able to obtain the semaphore, `nil' otherwise."
-  (if (cdr sem)
+(defun maybe-obtain-mutex (mtx)
+  "Attempt to obtain mutex MTX for the current thread without blocking.
+Returns `t' if able to obtain the mutex, `nil' otherwise."
+  (if (cdr mtx)
       nil
-    (obtain-semaphore sem)
+    (obtain-mutex mtx)
     t))
 
-(defun release-semaphore (sem)
-  "Release the semaphore object SEM (which should have previously been obtained
-by the current thread). Returns `t' if the semaphore has no new owner."
-  (or (eq (cdr sem) current-thread)
-      (error "Not owner of semaphore: %S, %S" sem current-thread))
-  (let
-      ((cell (cdr sem)))
-    (rplacd sem (cdr cell))
-    (if (cdr sem)
-	(progn
-	  (rplaca cell (car sem))
-	  (rplacd cell nil)
-	  (setq all-threads (nconc all-threads cell))
-	  nil)
-      t)))
+(defun release-mutex (mtx)
+  "Release the mutex object MTX (which should have previously been obtained
+by the current thread). Returns `t' if the mutex has no new owner."
+  (or (eq (cdr mtx) (current-thread))
+      (error "Not owner of mutex: %S" mtx))
+  (rplacd mtx (cddr mtx))
+  (if (cdr mtx)
+      (progn
+	(thread-wake (cadr mtx))
+	nil)
+    t))
