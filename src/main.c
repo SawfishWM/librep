79b665d539dc781a3e448fd21c6a233023e46ffa
john <john>
2000-03-13 21:43:50 +0000

    (rep_init_from_dump): call rep_macros_init ()

--- src/main.c
+++ src/main.c
@@ -236,6 +236,7 @@ rep_init_from_dump(char *prog_name, int *argc, char ***argv,
 
 	rep_values_init();
 	rep_lisp_init();
+	rep_macros_init ();
 	rep_lispcmds_init();
 	rep_lispmach_init();
 	rep_find_init();
