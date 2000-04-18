45698a37fe86b984c2cd663e37e4ff0fac64cbba
john <john>
2000-04-18 15:35:09 +0000

    *** empty log message ***

--- src/repint_subrs.h
+++ src/repint_subrs.h
@@ -41,6 +41,7 @@ extern void rep_find_kill(void);
 /* from lisp.c */
 extern repv rep_scm_t, rep_scm_f;
 extern repv rep_readl(repv, int *);
+extern repv rep_bind_lambda_list_1 (repv lambdaList, repv *args, int nargs);
 extern repv rep_bind_lambda_list(repv lambdaList, repv argList,
 				 rep_bool eval_args, rep_bool eval_in_env);
 extern repv rep_eval_lambda(repv, repv, rep_bool, rep_bool);
