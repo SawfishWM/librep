c618b54a4a3f72c381bee6887ec1d9348a782f36
john <john>
2000-06-01 15:21:42 +0000

    most macros just call expand-FOO

--- lisp/scheme/syntax.jl
+++ lisp/scheme/syntax.jl
@@ -26,146 +26,34 @@
 (define-structure scheme-syntax (export quote lambda if set! cond case
 					and or let let* letrec begin do
 					delay define)
-  ((open rep scheme-utils)
-   (access rep))
+    (open rep scheme-syntax-funs)
 
 ;;; syntax
 
-  (defmacro lambda (vars . body)
-    ((rep#lambda (header)
-       (while (eq (caar body) 'define)
-	 (setq header (cons (parse-define (car body)) header))
-	 (setq body (cdr body)))
-     (rep#cond
-      (header `(%lambda ,vars (letrec ,(nreverse header) ,@body)))
-      (t `(%lambda ,vars ,@body))))
-     nil))
+  (defmacro lambda args (apply expand-lambda args))
 
-  (defmacro if (test consequent . alternative)
-    (rep#cond
-     ((cdr alternative)
-      (error "Scheme `if' only takes one else form"))
-     (alternative
-      `(%cond ((%test ,test) ,consequent)
-	      ('t ,(car alternative))))
-     (t `(%cond ((%test ,test) ,consequent)))))
+  (defmacro if args (apply expand-if args))
 
-  (defmacro set! (variable expression)
-    `(%setq ,variable ,expression))
+  (defmacro set! args (apply expand-set! args))
 
-  (defmacro cond args
-    ((rep#lambda (first rest)
-      (rep#cond ((null args) #f)
-		((eq (car first) 'else)
-		 `(begin ,@(cdr first)))
-		((eq (cadr first) '=>)
-		 ((rep#lambda (tem)
-		   `(let ((,tem ,(car first)))
-		      (if ,tem
-			  (,(caddr first) ,tem)
-			,@(rep#cond (rest `((cond ,@rest)))))))
-		  (gensym)))
-		(t `(if ,(car first)
-			(begin ,@(cdr first))
-		      ,@(rep#cond (rest `((cond ,@rest))))))))
-     (car args) (cdr args)))
+  (defmacro cond args (apply expand-cond args))
 
-  (defmacro case (key . clauses)
-    (list* '%case key
-	  (mapcar (rep#lambda (x)
-		    (rep#cond
-		     ((eq (car x) 'else) `(t ,@(cdr x)))
-		     (t x))) clauses)))
+  (defmacro case args (apply expand-case args))
 
-  (defmacro or args
-    (rep#cond
-     ((null args) #f)
-     ((null (cdr args)) (car args))
-     (t ((rep#lambda (tem)
-	   `((lambda (,tem)
-	       (if ,tem ,tem (or ,@(cdr args))))
-	     ,(car args)))
-	 (gensym)))))
+  (defmacro or args (apply expand-or args))
 
-  (defmacro and args
-    (rep#cond
-     ((null args) '#t)
-     ((null (cdr args)) (car args))
-     (t `(cond (,(car args) (and ,@(cdr args))) (else #f)))))
+  (defmacro and args (apply expand-and args))
 
-  (defmacro let args
-    ((rep#lambda (fun vars values)
-       (rep#cond
-	((rep#cond ((car args) (symbolp (car args))))	;and expanded
-	 ;; named let
-	 (setq fun (car args))
-	 (setq args (cdr args))))
-       (setq vars (mapcar (rep#lambda (x)
-			    (rep#cond
-			     ((consp x) (car x))
-			     (t x)))
-			  (car args)))
-       (setq values (mapcar (rep#lambda (x)
-			      (rep#cond
-			       ((consp x) (cons 'begin (cdr x)))
-			       (t #f)))
-			    (car args)))
-       (rep#cond
-	(fun
-	 ;; use the progn so the compiler notices the inner letrec
-	 ;; (else it will get macroexpanded away too soon)
-	 (list 'begin
-	       (list 'letrec
-		     (list (list fun (list* 'lambda vars (cdr args))))
-		     (cons fun values))))
-	(t (cons (list* 'lambda vars (cdr args)) values))))
-     nil nil nil))
+  (defmacro let args (apply expand-let args))
 
-  (defmacro let* (bindings . body)
-    (rep#cond
-     ((null bindings) (cons 'begin body))
-     (t `((lambda (,(caar bindings)) (let* ,(cdr bindings) ,@body))
-	  ,(cadar bindings)))))
+  (defmacro let* args (apply expand-let* args))
 
-  (defmacro letrec (bindings . body)
-    ((rep#lambda (vars setters)
-       (list* 'let vars (nconc setters body)))
-     (mapcar (rep#lambda (x)
-	       (rep#cond
-		((consp x) (car x))
-		(t x)))
-	     bindings)
-     (mapcar (rep#lambda (x)
-	       (rep#cond
-		((consp x) (cons 'set! x))
-		(t (list 'set! x nil))))
-	     bindings)))
+  (defmacro letrec args (apply expand-letrec args))
 
-  (defmacro begin forms
-    (cons '%progn forms))
+  (defmacro begin forms (cons '%progn forms))
 
-  ;; initially I wrote this using while, to avoid the non-tail-recursive
-  ;; interpreter, but that doesn't create fresh bindings for each
-  ;; iteration
-  (defmacro do (vars test . body)
-    ((rep#lambda (tem)
-       `(let ,tem ,(mapcar (rep#lambda (var)
-			     (list (car var) (cadr var))) vars)
-	  (if ,(car test)
-	      (begin ,@(cdr test))
-	    (begin
-	     ,@body
-	     (,tem ,@(mapcar (rep#lambda (var)
-			       (rep#cond
-			        ((cddr var) (caddr var))
-			        (t (car var))))
-			     vars))))))
-     (gensym)))
+  (defmacro do args (apply expand-do args))
 
-  (defmacro delay (expression)
-    `(%make-promise (lambda () ,expression)))
+  (defmacro delay args (apply expand-delay args))
 
-  (defmacro define args
-    (rep#cond
-     ((symbolp (car args)) (cons 'set! args))
-     (t `(define ,(caar args) (lambda ,(cdar args) ,@(cdr args)))))))
+  (defmacro define args (apply expand-define args)))
