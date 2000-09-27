e61bf8e229df10f55651f9a0d319a8c7ba08dd40
john <john>
2000-09-27 16:36:19 +0000

    (strncasecmp): define if the system doesn't have it

--- src/misc.c
+++ src/misc.c
@@ -90,6 +90,33 @@ stpcpy(register char *dst, register const char *src)
 }
 #endif /* !HAVE_STPCPY */
 
+#ifndef HAVE_STRNCASECMP
+/* Compare no more than N characters of S1 and S2,
+   ignoring case, returning less than, equal to or
+   greater than zero if S1 is lexicographically less
+   than, equal to or greater than S2. (from glibc)  */
+int
+strncasecmp (const char *s1, const char *s2, size_t n)
+{
+  const unsigned char *p1 = (const unsigned char *) s1;
+  const unsigned char *p2 = (const unsigned char *) s2;
+  unsigned char c1, c2;
+
+  if (p1 == p2 || n == 0)
+    return 0;
+
+  do
+    {
+      c1 = tolower (*p1++);
+      c2 = tolower (*p2++);
+      if (c1 == '\0' || c1 != c2)
+        return c1 - c2;
+    } while (--n > 0);
+
+  return c1 - c2;
+}
+#endif
+
 u_char *
 rep_str_dupn(const u_char *old, int len)
 {
