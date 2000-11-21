d29298f7467bbc9f3fc8a966737a46ff8d9f1a52
John Harper <jsh@src.gnome.org>
2000-11-21 21:34:00 +0000

    rename repexecdir variable as repcommonexecdir so it doesn't conflict with
    	* rules.mk.sh: rename repexecdir variable as repcommonexecdir
    	so it doesn't conflict with Makedefs.in (shouldn't affect other
    	users of this file.. I hope)

--- rules.mk.sh
+++ rules.mk.sh
@@ -2,26 +2,26 @@
 # $Id$
 
 repdir=$1
-repexecdir=$2
+repcommonexecdir=$2
 repdocfile=$3
 
 cat <<EOF
 # rules.mk
 
 repdir=$repdir
-repexecdir=$repexecdir
-rpath_repexecdir=$repexecdir
+repcommonexecdir=$repcommonexecdir
+rpath_repcommonexecdir=$repcommonexecdir
 repdocfile=$repdocfile
 
-rep_LIBTOOL=\$(repexecdir)/libtool
-rep_INSTALL_ALIASES=\$(repexecdir)/install-aliases
+rep_LIBTOOL=\$(repcommonexecdir)/libtool
+rep_INSTALL_ALIASES=\$(repcommonexecdir)/install-aliases
 
 # use this like:
 # foo.la : foo.lo bar.lo
 #	\$(rep_DL_LD) link-opts...
 
 rep_DL_LD=\$(rep_LIBTOOL) --mode=link \$(CC) -avoid-version -module \
-	  -rpath \$(rpath_repexecdir) -export-symbols-regex '^rep_dl_'
+	  -rpath \$(rpath_repcommonexecdir) -export-symbols-regex '^rep_dl_'
 
 rep_DL_INSTALL=\$(rep_LIBTOOL) --mode=install \$(INSTALL)
 rep_DL_UNINSTALL=\$(rep_LIBTOOL) --mode=uninstall rm
