edc1e425427bad43f1dc30088222cedb33c98695
John Harper <jsh@pixelslut.com>
2001-06-28 05:25:00 +0000

    added some comments, some API changes
    2001-06-27  John Harper  <jsh@pixelslut.com>
    
    	* rep/net/rpc.jl: added some comments, some API changes

--- lisp/rep/net/rpc.jl
+++ lisp/rep/net/rpc.jl
@@ -64,6 +64,22 @@
 ;; returns
 
 
+;; Unlike CORBA it's not possible to transparently pass object
+;; references (proxies) over an RPC call, and have them work at the
+;; other end. 
+
+;; The solution is to convert the proxy to a global id, then pass that
+;; over the RPC call, so that the other side can convert it into a new
+;; proxy.
+
+;; It's also possible to pass references from servers behind firewalls
+;; (and thus can't create usable global ids). The solution here is to
+;; pass the local servant-id to the remote server, which can then use
+;; the remote-servant-id->rpc-proxy function. This knows that the given
+;; servant id refers to the connection used to invoke the currently
+;; executing rpc call
+
+
 ;; Todo:
 
 ;; - add a mechanism for async (`oneway') requests
@@ -80,8 +96,10 @@
 	    make-rpc-servant
 	    destroy-rpc-servant
 	    call-with-rpc-servant
-	    make-rpc-proxy
+	    rpc-proxy->global-id
+	    rpc-proxy->servant-id
 	    servant-id->global-id
+	    remote-servant-id->global-id
 	    global-id->rpc-proxy)
 
     (open rep
@@ -99,7 +117,12 @@
     (result-pending socket-result-pending socket-result-pending-set!)
     (closable socket-closable-p))
 
+  ;; The socket used to listen for connections to this server (or false)
   (define listener-socket nil)
+
+  ;; The socket that was used to invoke the innermost called servant
+  ;; implementation
+  (define active-socket (make-fluid))
 
 ;;; connection cache
 
@@ -109,18 +132,24 @@
   ;; maps from SOCKET -> SOCKET-DATA
   (define socket-data-table (make-weak-table eq-hash eq))
 
+  ;; Return the socket associated with SERVER:PORT. If there isn't one,
+  ;; try to connect to the server
   (define (server-socket server port)
     (or (table-ref socket-cache (cons server port))
 	(open-server server port)
 	(error ("No connection with server %s:%d" server port))))
 
   (define (register-rpc-server socket #!key closable)
+    "Add the connection SOCKET to the table of known rpc connections. If
+CLOSABLE is true, then the socket could be closed and reopened simply
+by knowing its address and port number."
     (let ((server (socket-peer-address socket))
 	  (port (socket-peer-port socket)))
       (table-set socket-cache (cons server port) socket)
       (table-set socket-data-table socket (make-socket-data closable))))
 
   (define (deregister-rpc-server socket)
+    "Remove SOCKET from the table of rpc connections."
     (let ((server (socket-peer-address socket))
 	  (port (socket-peer-port socket)))
       (when (eq (server-socket server port) socket)
@@ -130,11 +159,14 @@
 	  (table-unset socket-cache (cons server port))
 	  (table-unset socket-data-table socket)))))
 
+  ;; Return the data structure associated with SOCKET
   (define (socket-data socket) (table-ref socket-data-table socket))
 
 ;;; socket I/O
 
   (define (rpc-socket-listener master-socket)
+    "The function that should be used to listen for connections on rpc
+server sockets."
     (let (socket)
       (setq socket (socket-accept master-socket
 				  (lambda (output)
@@ -144,6 +176,7 @@
       (register-rpc-server socket #:closable nil)
       socket))
 
+  ;; Open an rpc connection to HOST:PORT
   (define (open-server host port)
     (let (socket)
       (setq socket (socket-client host port
@@ -155,6 +188,7 @@
       socket))
 
   (define (rpc-output-handler socket output)
+    "The function used to handle any OUTPUT from SOCKET."
     ;;(format standard-error "Read: %S\n" output)
     (let ((sock-data (socket-data socket)))
       (when (socket-pending-data sock-data)
@@ -183,9 +217,13 @@
 	      (t ;; Request
 	       (let ((result (call-with-exception-handler
 			      (lambda ()
-				(let ((id (car form))
+				(let ((impl (servant-ref (car form)))
 				      (args (cdr form)))
-				  (cons '#t (apply (servant-ref id) args))))
+				  (unless impl
+				    (error "No such RPC servant: %s"
+					   (car form)))
+				  (let-fluids ((active-socket socket))
+				    (cons '#t (apply impl args)))))
 			      (lambda (data)
 				(cons '#f data)))))
 		 ;;(format standard-error "Wrote: %S\n" result)
@@ -194,6 +232,8 @@
 	(when (< point (length output))
 	  (socket-pending-data-set! sock-data (substring output point))))))
 
+  ;; Wait for an rpc response on SOCKET. Parse it and either return the
+  ;; value or raise the exception
   (define (wait-for-reponse socket)
     (let ((old-vector (socket-result-pending (socket-data socket)))
 	  (result '()))
@@ -212,34 +252,44 @@
 	(raise-exception (cdr result)))))
 
   (define (rpc-create-server)
+    "Start listening for rpc connections on the current machine"
     (unless listener-socket
       (setq listener-socket (socket-server nil nil rpc-socket-listener))))
 
   (define (rpc-destroy-server)
+    "Stop listening for rpc connections on the current machine"
     (when listener-socket
       (close-socket listener-socket)
       (setq listener-socket nil)))
 
 ;;; servants
 
-  (define (make-servant-id)
-    (intern (concat #\= (number->string (current-utime) 36)
-		    #\= (number->string (random) 36) #\=)))
-
   ;; map from ID->RPC-IMPL
   (define servant-table (make-table eq-hash eq))
 
+  ;; Create a new (unique) servant id
+  (define (make-servant-id)
+    (intern (concat (number->string (current-utime) 36)
+		    #\- (number->string (random) 36))))
+
+  ;; Return the servant implementation associated with ID
   (define (servant-ref id) (table-ref servant-table id))
 
   (define (make-rpc-servant impl)
+    "Register the function IMPL as an rpc servant, and return the created
+servant-id."
     (let ((id (make-servant-id)))
       (table-set servant-table id impl)
       id))
 
   (define (destroy-rpc-servant id)
+    "Remove the servant with servant-id ID from the table of servants."
     (table-unset servant-table id))
 
   (define (call-with-rpc-servant impl callback)
+    "Call the function CALLBACK with a single argument, the servant-id that
+can be used to call the function IMPL. Once CALLBACK returns, the servant-id
+becomes invalid."
     (let ((id (make-rpc-servant impl)))
       (unwind-protect
 	  (callback id)
@@ -247,28 +297,63 @@
 
 ;;; proxies
 
-  (define (make-rpc-proxy server port servant-id)
+  ;; magic object used to get information from proxies
+  (define proxy-token (cons))
+
+  (define (make-proxy server port servant-id)
     (lambda args
-      (let ((socket (server-socket server port)))
-	;;(format standard-error "Wrote: %S\n" (cons servant-id args))
-	(write socket (prin1-to-string (cons servant-id args)))
-	(wait-for-reponse socket))))
+      (if (eq (car args) proxy-token)
+	  ;; when called like this, do special things
+	  (case (cadr args)
+	    ((global-id) (make-global-id server port servant-id))
+	    ((servant-id) servant-id))
+
+	;; otherwise, just forward to the server
+	(let ((socket (server-socket server port)))
+	  (write socket (prin1-to-string (cons servant-id args)))
+	  (wait-for-reponse socket)))))
+
+  (define (rpc-proxy->global-id proxy)
+    "Return the globally-valid servant-id (a string) that can be used to
+reference the RPC proxy function PROXY."
+    (proxy proxy-token 'global-id))
+
+  (define (rpc-proxy->servant-id proxy)
+    "Return the unqualified servant-id (a symbol) that can be used to
+reference the RPC proxy function PROXY."
+    (proxy proxy-token 'servant-id))
 
 ;;; globally referenceable ids
 
-  ;; returns a string
+  ;; Create the global servant id for ID@SERVER:PORT
+  (define (make-global-id server port id)
+    (format nil "%s@%s:%s" id server port))
+
   (define (servant-id->global-id id)
-    (or listener-socket (error "Need an opened RPC server"))
-    (format nil "%s:%s:%s"
-	    (socket-address listener-socket)
-	    (socket-port listener-socket) id))
+    "Return the globally referenceable RPC servant id for local servant id ID."
+    (unless listener-socket
+      (error "Need an opened RPC server"))
+    (make-global-id (socket-address listener-socket)
+		    (socket-port listener-socket) id))
+
+  (define (remote-servant-id->global-id id)
+    "Return the globally referenceable RPC servant id for the local servant
+id ID (a symbol) associated with the invoker of the currently active RPC
+request."
+    (unless (fluid active-socket)
+      (error "Not called from an RPC servant"))
+    (make-global-id (socket-peer-address (fluid active-socket))
+		    (socket-peer-port (fluid active-socket)) id))
 
   (define (global-id->rpc-proxy id)
-    (let ((bits (string-split ":" id)))
-      (let ((server (nth 0 bits))
-	    (port (string->number (nth 1 bits)))
-	    (servant-id (intern (nth 2 bits))))
-	(make-rpc-proxy server port servant-id))))
+    "Return a function that can be used to call the RPC associated with the
+global servant id ID (a string)."
+    (unless (string-looking-at "(.+)@(.+):(.+)" id)
+      (error "Badly formed global RPC servant id: %s" id))
+    (let ((servant-id (intern (expand-last-match "\\1")))
+	  (server (expand-last-match "\\2"))
+	  (port (string->number (expand-last-match "\\3"))))
+      (make-proxy server port servant-id)))
 
 ;;; initialization
 
