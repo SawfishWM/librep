c542e6028acd010b83e18e81b995146ae63965e2
Christopher Roy Bratusek <chris@nanolx.org>
2009-11-13 06:50:44 +0100

    OpenBSD fixups

--- src/numbers.c
+++ src/numbers.c
@@ -70,7 +70,7 @@ DEFSTRING(domain_error, "Domain error");
 #  define LONG_LONG_MIN LONGLONG_MIN
 #  define LONG_LONG_MAX LONGLONG_MAX
 # elif defined (LLONG_MIN)
-   /* Solaris uses LLONG_ */
+   /* Solaris and OpenBSD uses LLONG_ */
 #  define LONG_LONG_MIN LLONG_MIN
 #  define LONG_LONG_MAX LLONG_MAX
 # endif
